21d95045c98442c1a12e9e0c2c21db15dd7ccf07
anandbose79 <anand.bose@gmail.com>
2019-04-03 14:01:28 -0500

    removed comments

--- frontend-asia.py
+++ frontend-asia.py
@@ -1,25 +1,4 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-"""A simple web server which responds to HTTP GET requests by consuming CPU.
-This binary runs in a GCE VM. It serves HTTP requests on port 80. Every request
-with path '/service' consumes 1 core-second of CPU time, with the timeout of
-5 (walltime) seconds. The purpose of this application is to demonstrate how
-Google Compute Engine Autoscaler can scale a web frontend server based on CPU
-utilization.
-The original version of this file is available here:
-https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/compute/
-    autoscaler/demo/frontend.py
-"""
+
 
 import BaseHTTPServer
 from multiprocessing import Process
@@ -44,11 +23,7 @@ class CpuBurner(object):
             pass
 
     def burn_cpu(self):
-        """Consume REQUEST_CPUTIME_SEC core seconds.
-        This method consumes REQUEST_CPUTIME_SEC core seconds. If unable to
-        complete within REQUEST_TIMEOUT_SEC walltime seconds, it times out and
-        terminates the process.
-        """
+
         start_walltime_sec = self.get_walltime()
         start_cputime_sec = self.get_user_cputime()
         while (self.get_user_cputime() <
@@ -59,7 +34,6 @@ class CpuBurner(object):
                 sys.exit(1)
 
     def handle_http_request(self):
-        """Process a request to consume CPU and produce an HTTP response."""
         start_time = self.get_walltime()
         p = Process(target=self.burn_cpu)  # Run in a separate process.
         p.start()
@@ -77,10 +51,8 @@ class CpuBurner(object):
 
 
 class DemoRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    """Request handler for Demo http server."""
 
     def do_GET(self):
-        """Handle an HTTP GET request."""
         mapping = {
             "/": lambda: (200, "OK Asia"),  # Return HTTP 200 response.
             "/service": CpuBurner().handle_http_request,
